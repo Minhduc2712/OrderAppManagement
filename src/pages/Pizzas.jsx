a414464bd86acff05daea5462944d1a6c0258bb0
Minhduc2712 <115997551+Minhduc2712@users.noreply.github.com>
2023-11-02 10:16:45 +0700

    Update Pizzas.jsx

--- src/pages/Pizzas.jsx
+++ src/pages/Pizzas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import ProductCard from "../components/UI/product-card/ProductCard";
 import Helmet from "../components/Helmet/Helmet";
